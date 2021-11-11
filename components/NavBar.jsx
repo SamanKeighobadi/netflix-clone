bcadf4f8586a64275ce2123470ec3b930150b0cc
saman keighobadi <saman.keighobadi@gmail.com>
2021-11-11 11:56:28 +0330

    Navbar component

---
+++ components/NavBar.jsx
@@ -0,0 +1,22 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <div>
+                        <a className='uppercase text-3xl text-red-700 font-bold'>netflix</a>
+                    </div>
+                    <li>
+                        Language
+                    </li>
+                    <li>
+                        Sign in
+                    </li>
+                </ul>
+            </nav>            
+        </div>
+    );
+}
+
+export default Navbar;
